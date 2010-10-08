a1689843e5d4f5706680a39521561f037329bc33
Graylin Kim <graylin.kim@gmail.com>
2010-10-07 20:39:27 -0400

    Added the 3 servlets and their web.xml mappings that we'll need for this iteration

---
+++ src/main/java/gov/nysenate/opendirectory/servlets/PersonServlet.java
@@ -0,0 +1,23 @@
+package gov.nysenate.opendirectory.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class PersonServlet extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+	    out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 " +
+			"Transitional//EN\">\n" +
+			"<html>\n" +
+			"<head><title>Hello WWW</title></head>\n" +
+			"<body>\n" +
+			"<h1>Person!!</h1>\n" +
+			"</body></html>");
+	}
+}
