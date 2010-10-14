f86365da4b3e41a5588175227acc1293b4d1644f
Graylin Kim <graylin.kim@gmail.com>
2010-10-13 22:14:28 -0400

    Restructures BrowseServlet to use TreeSets for sorting and iterating through elements, now passes restructured data to all 4 browse jsps.

--- src/main/java/gov/nysenate/opendirectory/servlets/BrowseServlet.java
+++ src/main/java/gov/nysenate/opendirectory/servlets/BrowseServlet.java
@@ -5,11 +5,13 @@ import gov.nysenate.opendirectory.models.Person;
 
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
  
 import javax.naming.NamingException;
 import javax.servlet.ServletException;
@@ -20,96 +22,98 @@ import javax.servlet.http.HttpServletResponse;
 @SuppressWarnings("serial")
 public class BrowseServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		StringTokenizer tokens = new StringTokenizer(request.getRequestURI(),"/");
-		tokens.nextToken(); //Throw `OpenDirectory` away
-	    tokens.nextToken(); //Throw `browse` away
-	    if (tokens.hasMoreTokens()) {
-	    	String type = tokens.nextToken().toLowerCase();
-	    	if ( type.equals("department")) {
-	    		BrowseByDepartment(request,response);
-	    	} else if ( type.equals("firstname") ) {
-    			BrowseByFirstName(request,response);
-	    	} else if ( type.equals("lastname") ) {
-	    		BrowseByLastName(request,response);
-	    	} else if ( type.equals("location") ) {
-	    		BrowseByLocation(request,response);
-	    	}
-	    } else {
-	    	getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
-	    }
-	}
-	
-	private void BrowseByDepartment(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		getServletContext().getRequestDispatcher("/dept.jsp").forward(request, response);
-	}
-	private void BrowseByLocation(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		getServletContext().getRequestDispatcher("/loc.jsp").forward(request, response);
-	}
-	private void BrowseByLastName(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		try {
-			long start = System.currentTimeMillis();
-			ArrayList<Person> people = (ArrayList<Person>)new Ldap().connect().getPeople();
-			long stop = System.currentTimeMillis();
-			System.out.print("Retrieval in: "+(stop-start));
-			
-			start = System.currentTimeMillis();
-    		HashMap<String,ArrayList<Person>> data = new HashMap<String,ArrayList<Person>>();
-    		for(Person p : people) {
-    			InsertPersonByString(p.getLastName(), p, data);
-    		}
-    		for(ArrayList<Person> peeps : data.values()) {
-    			Collections.sort(peeps, new Person.ByLastName());
-    		}
-    		stop = System.currentTimeMillis();
-    		System.out.println("Sorted in: "+(stop-start));
-    		
-    		request.setAttribute("people", data);
-    		getServletContext().getRequestDispatcher("/last.jsp").forward(request, response);
-		} catch (NamingException e) {
+		try{
+			StringTokenizer tokens = new StringTokenizer(request.getRequestURI(),"/");
+			tokens.nextToken(); //Throw `OpenDirectory` away
+		    tokens.nextToken(); //Throw `browse` away
+		    if (tokens.hasMoreTokens()) {
+		    	String type = tokens.nextToken().toLowerCase();
+		    	if ( type.equals("department")) {
+					request.setAttribute(
+							"people",
+							GetPeopleSortedBy(
+									Person.class.getMethod("getDepartment"),
+									new Person.ByDepartment()
+								)
+						);
+					getServletContext().getRequestDispatcher("/dept.jsp").forward(request, response);
+					
+		    	} else if ( type.equals("firstname") ) {
+					request.setAttribute(
+							"people",
+							GetPeopleSortedBy(
+									Person.class.getMethod("getFirstName"),
+									new Person.ByFirstName()
+								)
+						);
+					getServletContext().getRequestDispatcher("/first.jsp").forward(request, response);
+					
+		    	} else if ( type.equals("lastname") ) {
+					request.setAttribute("people",
+							GetPeopleSortedBy(
+								Person.class.getMethod("getLastName"),
+								new Person.ByLastName())
+						);
+		    		getServletContext().getRequestDispatcher("/last.jsp").forward(request, response);
+		    		
+		    	} else if ( type.equals("location") ) {
+					request.setAttribute(
+							"people",
+							GetPeopleSortedBy(
+									Person.class.getMethod("getLocation"),
+									new Person.ByLocation()
+								)
+						);
+					getServletContext().getRequestDispatcher("/loc.jsp").forward(request, response);
+					
+		    	}
+		    } else {
+		    	getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
+		    }
+		} catch (NoSuchMethodException e) {
+			//If the method by name doesn't exist
+			System.out.println(e);
+		} catch (SecurityException e) {
+			//If the method isn't gettable I think? not sure what `security` means
 			System.out.println(e);
 		}
 	}
-	
-	private void BrowseByFirstName(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	private HashMap<String,TreeSet<Person>> GetPeopleSortedBy(Method method, Comparator<Person> comparator) {
 		try {
 			long start = System.currentTimeMillis();
 			ArrayList<Person> people = (ArrayList<Person>)new Ldap().connect().getPeople();
-			long stop = System.currentTimeMillis();
-			System.out.print("Retrieval in: "+(stop-start));
-			
-			start = System.currentTimeMillis();
-    		HashMap<String,ArrayList<Person>> data = new HashMap<String,ArrayList<Person>>();
-    		for(Person p : people) {
-    			InsertPersonByString(p.getFirstName(),p,data);
-    		}
-    		stop = System.currentTimeMillis();
-    		System.out.println("Bucketed in: "+(stop-start));
-    		
-    		start = System.currentTimeMillis();
-    		for(ArrayList<Person> peeps : data.values()) {
-    			Collections.sort(peeps, new Person.ByFirstName());
-    		}
-    		stop = System.currentTimeMillis();
-    		System.out.println("Sorted in: "+(stop-start));
-    		
-    		request.setAttribute("people", data);
-    		getServletContext().getRequestDispatcher("/first.jsp").forward(request, response);
-		} catch (NamingException e) {
-			System.out.println(e);
-		}
-	}
-	
-	private void InsertPersonByString(String name, Person p, HashMap<String,ArrayList<Person>> data) {
-		if(name!=null && name.length()!=0) {
-			name = name.toUpperCase();
-			ArrayList<Person> plist = data.get(String.valueOf(name.charAt(0)));
-			if (plist == null) {
-				plist = new ArrayList<Person>();
-				data.put((String.valueOf(name.charAt(0))), plist);
+			System.out.println("LDAP Query Time: "+(System.currentTimeMillis()-start)+" milliseconds");
+			HashMap<String,TreeSet<Person>> data = new HashMap<String,TreeSet<Person>>();
+			for(Person p : people) {
+				try {
+					String value = (String)method.invoke(p);
+					if (value!=null && value.length()!=0) {
+						value = value.toUpperCase();
+						TreeSet<Person> plist = data.get(String.valueOf(value.charAt(0)));
+						if (plist == null) {
+							plist = new TreeSet<Person>(comparator);
+							data.put((String.valueOf(value.charAt(0))), plist);
+						}
+						System.out.println(p.getFullName());
+						plist.add(p);
+					}
+				} catch (NullPointerException e) {
+					//Person doesn't have a `value`
+					e.printStackTrace();
+				}
 			}
-			plist.add(p);
-		} else {
-			//Person has no name?!?!
+			return data;
+		} catch (NamingException e) {
+			//Error Connecting to LDAP
+			System.out.println(e);
+		} catch (InvocationTargetException e) {
+			//Person is not a valid target for the method
+			System.out.println(e);
+		} catch (IllegalAccessException e) {
+			//Method is private and not accessible
+			System.out.println(e);
 		}
+		return null;
 	}
 }
