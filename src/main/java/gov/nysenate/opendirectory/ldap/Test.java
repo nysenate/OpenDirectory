b846333fdf8c294f0d63decb766146b899f51a1c
chrim <zoner16@gmail.com>
2010-10-15 12:58:40 -0400

    Successful write and read to solr

--- src/main/java/gov/nysenate/opendirectory/ldap/Test.java
+++ src/main/java/gov/nysenate/opendirectory/ldap/Test.java
@@ -1,7 +1,7 @@
 package gov.nysenate.opendirectory.ldap;
 
 import gov.nysenate.opendirectory.models.Person;
-
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -19,6 +19,7 @@ import javax.naming.directory.SearchResult;
 
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.CommonsHttpSolrServer;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocumentList;
@@ -33,51 +34,10 @@ public class Test {
 	 * @throws NamingException
 	 */
 	public static void main(String[] args) throws NamingException {
-		
-		try {
-			/*********SOLR Test**********/
-			CommonsHttpSolrServer localserver = null;
-			//Could use EmbeddedSolrServer(), depends on how we are setting up the environments?
-			localserver = new CommonsHttpSolrServer("http://localhost:8080/solr/");
-			
-			SolrServer server = localserver;
-		
-			Person test_p = new Person();
-			
-			test_p.setEmail("test_email@chrim.com");
-			test_p.setFirstName("test");
-			test_p.setLastName("person");
-			test_p.setDepartment("testDepartment");
-			test_p.setFullName("test person");
-			test_p.setLocation("Albany");
-			test_p.setPhone("123-333-1111");
-			test_p.setState("NY");
-			test_p.setTitle("Manager");
-			test_p.setUid("1");
-			
-			server.addBean(test_p);
-			server.commit();
-			
-			
-			SolrQuery query = new SolrQuery();
-			query.setQuery("*:*");
-			
-			QueryResponse rsp = server.query(query);
-		
-			ArrayList<Person> beans = (ArrayList<Person>)rsp.getBeans(Person.class);
-			
-			for(Person p : beans) {
-				System.out.println(p.getUid());
-			}
-			
-		}
-		catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		/*******PULL in INFO from LDAP*********
+	
 		try {				
+			
+			/*******PULL in INFO from LDAP*********/
 			//Set the attributes to retrieve
 			String[] attributestoretrieve = {"displayname","location","givenname","uidnumber",
 											"uid", "mail", "cn", "telephonenumber",
@@ -87,10 +47,11 @@ public class Test {
 			SearchControls controls = new SearchControls();
 			controls.setReturningAttributes(attributestoretrieve);
 			
+			
 			//Connection credentials (null,null) = anonymous
 			String cred = null;
 			String pass = null;
-			
+
 			//The DirContext represents our connection with ldap
 			DirContext ldap = getLdap(cred,pass);
 			
@@ -98,56 +59,101 @@ public class Test {
 			String domainFilter = "O=senate"; 			//organization = senate
 			//String domainFilter = new String();
 			//String searchFilter = "(objectClass=dominoPerson)"; 	//all users
-			String searchFilter = "(displayname=Andrew H*)"; 
+			//String searchFilter = "(displayname=Andrew H*)"; 
 			//String searchFilter = "(uid=CRM*)"; //all "CRM" users
 			//String searchFilter = "(&(objectClass=dominoGroup)(giddisplay=Public))";
-			//String searchFilter = "(&(objectClass=dominoPerson)(employeeid=*)(!(employeeid=999*))(!(employeeid=0000)))"; //clean query
+			String searchFilter = "(&(objectClass=dominoPerson)(employeeid=*)(!(employeeid=999*))(!(employeeid=0000)))"; //clean query
 			
+			long start = System.currentTimeMillis();
 			//Execute our search over the `O=senate` ldap domain 
 			NamingEnumeration<SearchResult> results = ldap.search(domainFilter,searchFilter,controls);
-					
-			//Iterate through our results
-			int resultNum = 1;
+			System.out.println("Searching takes: "+(System.currentTimeMillis()-start)+" milliseconds");
+			
+			/*****Create solr server connection********/
+			CommonsHttpSolrServer localserver = null;
+			localserver = new CommonsHttpSolrServer("http://localhost:8080/solr/");
+			
+			
+			/*
+			start = System.currentTimeMillis();
+			SolrServer server = localserver;
+			server.deleteByQuery("*:*");
+			System.out.println("Deletion takes: "+(System.currentTimeMillis()-start)+" milliseconds");
+			
+			
+			//Iterate through our results, create Person objects, add them to solr
 			System.out.println("Results for query: `"+searchFilter+"`");
+			ArrayList<Person> people_to_add = new ArrayList();
+			int resultNum=1;
 			while (results.hasMore()) {
-				
-				SearchResult result = results.next();
-				
-				//Get the result attributes and all of their IDs
-				Attributes attributes = result.getAttributes();
-				NamingEnumeration<String> ids = result.getAttributes().getIDs();
-				
-				//Iterate through our attributes
-				System.out.println("Result "+resultNum+": "+result.getName());
-				while( ids.hasMore() ) {
-					
-					//Print Results
-					String id = ids.next();
-					//Get all the values for that attribute (could be a list)
-					NamingEnumeration<?> values = attributes.get(id).getAll();
-					
-					//Iterate through those values 
-					StringBuilder row = new StringBuilder("\t").append(id).append(": ");
-				
-					while(values.hasMore()) {
-						row.append(values.next());
-						if (values.hasMore()) {
-							row.append(", ");
-						}	
-					}
-				
-					System.out.println(row);
-				}
-				
-				resultNum++;
+				System.out.println(resultNum++);
+				people_to_add.add(new Person(results.next()));
 			}
-		
+			
+			server.addBeans(people_to_add);
+			server.commit();
+			*/
+			start = System.currentTimeMillis();
+			
+			SolrServer server = localserver;
+			
+			
+			SolrQuery query = new SolrQuery();
+			query.setQuery("*:*");
+			query.setRows(1900);
+			
+			QueryResponse rsp = server.query(query);
+			System.out.println("Querying takes: "+(System.currentTimeMillis()-start)+" milliseconds");
+			
+			System.out.println(rsp.getResults().size());
+			
+			/*
+			
+			//Get the result attributes and all of their IDs
+			//Attributes attributes = result.getAttributes();
+			//NamingEnumeration<String> ids = result.getAttributes().getIDs();
+			
+			//Iterate through our attributes
+			//System.out.println("Result "+resultNum+": "+result.getName());
+			while( ids.hasMore() ) {
+				
+				String id = ids.next();
+				//Get all the values for that attribute (could be a list)
+				NamingEnumeration<?> values = attributes.get(id).getAll();
+				
+				//Print Results
+				String id = ids.next();
+				//Get all the values for that attribute (could be a list)
+				NamingEnumeration<?> values = attributes.get(id).getAll();
+				
+				//Iterate through those values 
+				StringBuilder row = new StringBuilder("\t").append(id).append(": ");
+			
+				while(values.hasMore()) {
+					row.append(values.next());
+					if (values.hasMore()) {
+						row.append(", ");
+					}	
+				}
+			
+				System.out.println(row); 
+			}
+			*/
+			
 		} 
 		//If the authorization credentials are bad, we'll catch that here and report the failure
 		catch (AuthenticationException e) {
 			System.out.println("Authentication Failed!");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SolrServerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} 
-		*/
 		
 		//Bad queries aren't caught here, not sure how to do that yet.
 	}
