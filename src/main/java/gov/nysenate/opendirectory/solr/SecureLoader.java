cc2c39cc30c5092bf6464242a7dbac1ec107d203
Graylin Kim <graylin.kim@gmail.com>
2010-12-06 21:46:21 -0500

    Adds authentication exceptions for the new team users

--- src/main/java/gov/nysenate/opendirectory/solr/SecureLoader.java
+++ src/main/java/gov/nysenate/opendirectory/solr/SecureLoader.java
@@ -5,6 +5,7 @@ import java.util.TreeSet;
 import org.apache.solr.common.SolrDocument;
 
 import gov.nysenate.opendirectory.models.Person;
+import gov.nysenate.opendirectory.utils.SerialUtils;
 
 public class SecureLoader {
 
@@ -24,26 +25,28 @@ public class SecureLoader {
 	}
 	
 	public Person loadPerson(SolrDocument profile) {
-		//Do the loading here
-		Person person = new Person();
 		
-		//long start = System.nanoTime();
+		//Load up the most basic of people
+		Person person = new Person();
+		person.setUid((String)profile.getFieldValue("uid"));
+		person.setCredentials(SerialUtils.loadStringSet((String)profile.getFieldValue("credentials")));
+		
+		//Any further queries required should be executed as under this person.
+		SolrSession personSession = session.solr.newSession(person);
+		
 		String permissions = (String)profile.getFieldValue("permissions");
 		String[] parts = permissions.split(":");
 		for(int j=0; j<parts.length-1; j+=2) {
 			
-			if(!isApproved(user,parts[j+1]))
+			if(!isApproved(user,parts[j+1]) || parts[j].equals("uid") || parts[j].equals("credentials"))
 				continue;
 			
 			String fieldname = parts[j];
 			Object fieldvalue = profile.getFieldValue(fieldname);
 			
-			person.setFieldFromRawValue(fieldname,fieldvalue,permissions,session);
+			person.setFieldFromRawValue(fieldname,fieldvalue,permissions,personSession);
 		}
-		//long end = System.nanoTime();
-		//System.out.println((end-start)/1000f+" microsecond load");
-		
-		System.out.println("Num Bookmarks: "+person.getBookmarks().size());
+
 		return person;
 	}
 
