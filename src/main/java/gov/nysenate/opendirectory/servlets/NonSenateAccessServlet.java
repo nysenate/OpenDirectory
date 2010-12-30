8841767a84435ce39bbd19540c01de3c227d6cef
Jared Williams <williams@nysenate.gov>
2010-12-30 11:28:46 -0500

    adding the ability to login from outside of the senate network

---
+++ src/main/java/gov/nysenate/opendirectory/servlets/NonSenateAccessServlet.java
@@ -0,0 +1,105 @@
+package gov.nysenate.opendirectory.servlets;
+
+import gov.nysenate.opendirectory.ldap.Ldap;
+import gov.nysenate.opendirectory.servlets.UserServlet.UserServletException;
+import gov.nysenate.opendirectory.utils.Request;
+
+import java.io.IOException;
+
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+public class NonSenateAccessServlet extends BaseServlet {
+	private static final long serialVersionUID = 1L;
+	
+	public class ExternalServletException extends Exception {
+		public ExternalServletException(String m) { super(m); }
+		public ExternalServletException(String m, Throwable t) { super(m,t); }
+	}
+       
+    public NonSenateAccessServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Request self = new Request(this,request,response);
+		self.render("access.jsp");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Request self = new Request(this,request,response);
+	    String command = urls.getCommand(request);
+	    
+	    try {
+		    //If they are posting to a bad place, 
+		    if(command == null)
+		    	throw new ExternalServletException("No command supplied to POST request.");
+		    
+	    	if(command.equals("login")) {
+	    		doLogin(self);
+	    		
+	    	}
+		} catch (ExternalServletException e) {
+	    	doException(self,e);
+	    }
+	}
+	    
+	public void doException(Request self, ExternalServletException e) throws ServletException, IOException {
+		System.out.println(e.getMessage());
+		e.printStackTrace();
+		if(e.getCause()!=null)
+			e.getCause().printStackTrace();
+		self.redirect(urls.url("external"));
+	}
+	
+	public void doLogin(Request self) throws ExternalServletException, ServletException, IOException {
+		String cred = ((String)self.httpRequest.getParameter("name")).toLowerCase();
+		String pass = (String)self.httpRequest.getParameter("password");
+		
+		//If they are already logged in, forward to their profile
+		if(self.httpSession.getAttribute("uid") != null ) {
+			self.redirect(urls.url("person",cred));
+			
+		// Check to see the user attempting to log in is valid before authenticating
+		// This needs to be done because not all authenticating users are in the OpenDirectory
+		} else if (self.solrSession.loadPersonByUid(cred) != null) {
+    		try {
+    			
+    			//Attempt to authenticate and login the user with the credentials supplied
+    			//Make temporary exceptions for non LDAP records (for testing)
+    			if ( /*(cred.equalsIgnoreCase("opendirectory") && pass.equals("senbook2010"))
+    					|| (cred.equalsIgnoreCase("graylin") && pass.equals("graylin1"))
+    					|| */ Ldap.authenticate(cred,pass)) {
+    				self.httpSession.setAttribute("uid",cred);
+    				self.redirect(urls.url("person",cred,"profile"));
+    				
+    			//Otherwise, alert them that their combination was wrong and redirect them to try again
+    			} else {
+	    			self.httpRequest.setAttribute("errorMessage", "Username and/or password were incorrect.");
+	    			self.render("/access.jsp");
+    			}
+    			
+    		//There is a small chance that a connection to LDAP is not available for some reason
+    		} catch (NamingException e) {
+    			
+    			//Record the Error to our logs
+    			e.printStackTrace();
+    			
+    			//Alert the user to the situation
+    			self.httpRequest.setAttribute("errorMessage", "The ldap server is down and you can't be authenticated. Please try again later.");
+    			self.render("access.jsp");
+    		}
+		
+		//Person is not in our database
+		} else {
+			self.httpRequest.setAttribute("errorMessage", "The user you have specified `"+cred+"` is not indexed in OpenDirectory.");
+			self.render("access.jsp");
+		}
+
+	}
+}
---
+++ src/main/webapp/jsp/access.jsp
@@ -0,0 +1,56 @@
+<%@ page language="java" import="gov.nysenate.opendirectory.models.Person,gov.nysenate.opendirectory.utils.UrlMapper" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%><%
+	UrlMapper urls = (UrlMapper)request.getAttribute("urls");
+	String error_message = (String)request.getAttribute("errorMessage");
+	Person user = (Person)request.getAttribute("user");
+	String title = (String)request.getAttribute("title");
+	
+%>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title><%=title%></title>
+		<link rel="stylesheet" type="text/css" href="<%=urls.url("css","application.css")%>" />
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+		<script type="text/javascript" src="<%=urls.url("js","application.js")%>"></script>
+	</head>
+	<body>
+		<div id="page">
+			<div id="header">
+				<a href="http://www.nysenate.gov">
+					<img src="http://www.nysenate.gov/sites/all/themes/nys/images/nyss_logo.png" id="logo" />
+				</a>
+			</div>
+			<div id="nav_bar">
+			</div>
+			<div id="main" style="margin-top:50px;">
+				<div id="main_center">					
+					<% if(error_message != null) { %>
+						<div id="error">
+							<%=error_message %>
+							<br></br>
+						</div>
+					<% } %>
+					
+					<div id="edit_form_field">
+						<form action="<%=urls.url("external","login")%>" method="POST">
+							<ol>
+								<li>
+									<label for="login_name">Username:</label>
+									<input type="text" name="name" size="31" maxlength="255" value="" id="login_name"/>
+								</li>
+								<li>
+									<label for="login_pword">Password:</label>
+								<input type="password" name="password" size="31" maxlength="255" value="" id="login_pword"/>	
+								</li>					
+							</ol>
+							<input type="submit" value="Login" id="login_button"></input>
+						</form>
+					</div>
+				</div>
+			</div>
+			<div id="footer">
+			</div>
+		</div>
+	</body>
+</html>
